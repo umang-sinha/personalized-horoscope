ff8d912ea746ad7cccad9e903cfa10db0d9c453a
umang-sinha <umangsinha12@gmail.com>
2025-07-19 20:01:03 +0530

    refactor: change userId type to string

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { env } from "../config/env";
 
-export function generateToken(userId: number) {
+export function generateToken(userId: string) {
   return jwt.sign({ userId }, env.JWT_SECRET, { expiresIn: "1d" });
 }
